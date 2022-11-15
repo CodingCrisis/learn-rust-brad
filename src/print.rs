1e31656d7b929f7f842be0c2febc809cbb04036c
Coding Crisis <107774580+CodingCrisis@users.noreply.github.com>
2022-11-15 23:29:37 +0000

    Hello world and some prints - thanks Brad!

---
+++ src/print.rs
@@ -0,0 +1,24 @@
+pub fn run(){
+    //Hello world
+    println!("Hello, from a function!");
+    
+    //Print with args
+    println!("{} is playing with {}", "Marcin", "Rust");
+    
+    //Print with positional args
+    println!("{0} is playing with {1} arguments in {2}", "Marcin", "positional", "Rust");
+
+    //Print with named args
+    println!(
+        "{name} is playing with {typeofarg} arguments in {language}",
+        name = "Marcin",
+        typeofarg = "named",
+        language = "Rust"
+    );
+
+    //Print with number formatting
+    println!("{0} in Hex is \"{0:X}\", but \"{0:o}\" in octal", 10);
+
+    //Print a tuple
+    println!("{:?}", ("Marcin", true, 41));
+}

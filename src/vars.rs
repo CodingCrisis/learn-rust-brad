d01fd47f6a3cafc83fce5c13918542070b1393f9
codingcrisis <codingcrisis@proton.me>
2023-01-04 23:22:09 +0100

    Starting to work with variables

---
+++ src/vars.rs
@@ -0,0 +1,10 @@
+// Let's play with variables
+
+pub fn run() {
+    let name = "Marcin";
+    let mut age = 41;
+
+    println!("My name is {} and I am {}", name, age);
+    age = 42;
+    println!("Nah, I'm actually {}", age);
+}
